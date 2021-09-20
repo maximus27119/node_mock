0ffd73356ffa627997e6d3c3f06cd99a02d0c9ad
Maksym <max.taina32@gmail.com>
2021-09-20 17:56:04 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
